b0aeaf0dcef14789b0a714940ac5de84dab6fe22
Matus Kovac <matus.kovac2@gmail.com>
2020-11-23 21:42:23 +0100

    init

---
+++ Assets/_Complete-Game/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class ScoreManager : MonoBehaviour
+    {
+        public static float score;        // The player's score.
+
+
+        Text text;                      // Reference to the Text component.
+
+
+        void Awake ()
+        {
+            // Set up the reference.
+            text = GetComponent <Text> ();
+
+            // Reset the score.
+            score = 0;
+        }
+
+
+        void Update ()
+        {
+            // Set the displayed text to be the word "Score" followed by the score value.
+            score += Time.deltaTime;
+            text.text = "Score: " + (int)score;
+        }
+    }
+}
